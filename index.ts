917fce000b189e7fa25f578629fc5632b5896059
ZvenRamm <miguel.rosales@alumno.uttehuacan.edu.mx>
2023-10-18 21:49:28 -0600

    V1.1

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 const express = require("express")
 const app = express();
+import router from './src/routes'
 
 const example = function(){
     console.log("Estoy a la escucha");
 }
+
+router(app);
 
 app.listen(
     9000,example
